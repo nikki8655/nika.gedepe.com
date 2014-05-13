66cfcb79ffbe493424470dc3bb36a1ef38aa4be8
Greg Zorbov <gregzorb@gmail.com>
2014-05-13 16:25:03 +0300

    Change bad comments

--- library/Controllers/AdminController.php
+++ library/Controllers/AdminController.php
@@ -5,7 +5,7 @@
  *
  * @author root
  */
-require_once 'AppController.php'; // подключение файла
+require_once 'AppController.php';
 
 class AdminController extends AppController {
 
@@ -13,8 +13,8 @@ class AdminController extends AppController {
     protected $DB = NULL;
 
     function __construct() {
-        echo date("Y-m-d H:i:s"); // вызвали ехо с датой
-        $this->result = $this->readFile('Admin/layout.phtml'); // что бы открывлся layout.phtml
+        echo date("Y-m-d H:i:s");
+        $this->result = $this->readFile('Admin/layout.phtml');
         include_once __DIR__ . '/../Lib/DB.php';
         $this->DB = new DB();
     }
@@ -134,7 +134,7 @@ class AdminController extends AppController {
         $page = $this->readFile('Admin/' . __FUNCTION__ . '.phtml');
         $body = "";
 
-        //Проверяем есть ли идентификатор категории в запросе
+        //Check id in query
         $id = 0;
         if (isset($_GET['id'])) {
             $id = (int) $_GET['id'];
@@ -144,13 +144,13 @@ class AdminController extends AppController {
 
         if (empty($_POST)) {
             if ($id > 0) {
-                // Вставлять данные из базы
+                // Get data from DB
                 $db_result = $this->DB->query("SELECT * FROM categories WHERE id = " . (int) $_GET['id']);
                 while ($row = mysql_fetch_assoc($db_result)) {
                     $body .= str_replace(array('{ID}', '{TITLE}', '{DESCRIPTION}'), array($row['id'], $row['title'], $row['description']), $page);
                 }
             } else {
-                // Показывать пустые поля
+                // Show empty fields
                 $body .= str_replace(array('{ID}', '{TITLE}', '{DESCRIPTION}'), array(), $page);
             }
         } else {
@@ -168,11 +168,11 @@ class AdminController extends AppController {
             if (!$id > 0) {
                 $id = $db_res;
             }
-            // Вставляем данные, которые мы передали в форме
+            // Insert data to template
             $body .= str_replace(array('{ID}', '{TITLE}', '{DESCRIPTION}'), array($id, $row['title'], $row['description']), $page);
         }
 
-        //Обработка ошибок и сообщений
+        // Work with errors and messages
         $body = str_replace(array('{ERROR_MSG}', '{SUCCESS_MSG}'), array($error_message, $success_message), $body);
         $this->result = str_replace('{BODY}', $body, $this->result);
         return $this->result;
